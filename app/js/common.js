6a5ab1ec4e9b76e13abada7669135b4ab0771956
Bogdan <smarovbogdan1@gmail.com>
2019-02-19 14:36:58 +0200

    верстка до БЕМа

--- app/js/common.js
+++ app/js/common.js
@@ -17,45 +17,6 @@ $(function() {
 		$('.hamburger').removeClass('is-active');
 	});
 
-	$('.carousel-service-composition h3').each(function() {
-		var ths = $(this);
-		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
-	});
-
-	$('section h2').each(function() {
-		var ths = $(this);
-		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
-	});
-
-	$('.carousel-services').owlCarousel({
-		loop: true,
-		nav: true,
-		smartSpeed: 700,
-		navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
-		responsiveClass: true,
-		responsive: {
-			0: {
-				items: 1,
-			},
-			800: {
-				items: 2,
-			},
-			1100: {
-				items: 3,
-			}
-		}
-	}).on('changed.owl.carousel', function() {
-		carouselService();
-	});
-
-	function carouselService() {
-		$('.carousel-services-item').each(function() {
-			var ths = $(this);
-			var thsh = ths.find('.carousel-services-content').outerHeight();
-			ths.find('.carousel-services-image').css('min-height', thsh);
-		});
-	}carouselService();
-
 	$('.reviews').owlCarousel({
 		loop: true,
 		items: 1,
@@ -63,43 +24,25 @@ $(function() {
 		nav: false
 	});
 
-	$('.partners').owlCarousel({
-		loop: true,
-		smartSpeed: 700,
-		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		nav: true,
-		responsiveClass: true,
-		responsive: {
-			0: {
-				items: 1,
-			},
-			768: {
-				items: 2,
-			},
-			992: {
-				items: 3,
-			},
-			1200: {
-				items: 4,
-			}
-		}
-	});
-
 	$('select').selectize({
 		create: true,
 		sortField: 'text'
 	});
 
-	$('.top').click(function() {
+	$('.top-button').click(function() {
 		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 	});
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > $(window).height()) {
-			$('.top').addClass("active");
+			$('.top-button').addClass("active");
 		} else {
-			$('.top').removeClass("active");
+			$('.top-button').removeClass("active");
 		};
 	});
+
+	//Equal Height
+	$('.s-specialization .wrapper').equalHeights();
+
 
 	//E-mail Ajax Send
 	$("form.callback").submit(function() { //Change
